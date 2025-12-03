deef15b1be607eb8772247e674dc37fecea81c1a
Aziz <elizadeeziz2006@gmail.com>
2025-12-03 18:56:51 +0400

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,17 +3,16 @@
 /**
  * main - Entry point
  *
- * Description: Prints "with proper grammar, but the outcome
- *              is a piece of art," followed by a new line
- *              using the printf function.
+ * Description: Prints the size of various types on the computer
+ *              it is compiled and run on.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s) \n", sizeof(char));
-	printf("Size of an int: %d byte(s) \n", sizeof(int));
-	printf("Size of a long int: %d byte(s) \n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s) \n", sizeof(long long int));
-	printf("Size of a float: %d byte(s) \n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
