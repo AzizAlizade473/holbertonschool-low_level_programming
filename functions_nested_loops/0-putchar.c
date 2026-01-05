b9fae13b940e3ea9f168ad1bcf8b471d38ae25be
AzizAlizade473 <elizadeeziz2006@gmail.com>
2026-01-05 11:56:04 +0000

    sds

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
 #include "main.h"
-int main(void) {
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -10,7 +18,6 @@ int main(void) {
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-    
 
-    return 0;
-}
+	return (0);
+}}
