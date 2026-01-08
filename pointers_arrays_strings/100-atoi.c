d525e74599548800e90d71bc390432c6dcceee20
AzizAlizade473 <elizaeeziz2006@gmail.com>
2026-01-08 16:49:23 +0000

    dsdsd

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,28 +14,22 @@ int _atoi(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		
 		if (s[i] == '-')
 		{
 			sign *= -1;
 		}
 
-		
 		if (s[i] >= '0' && s[i] <= '9' && sign == -1)
 		{
 			temp = temp * 10 + (s[i] - '0');
-
 			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
 			{
 				return (-temp);
 			}
 		}
-
-		
 		else if (s[i] >= '0' && s[i] <= '9' && sign == 1)
 		{
 			temp = temp * 10 + (s[i] - '0');
-
 			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
 			{
 				return (temp);
