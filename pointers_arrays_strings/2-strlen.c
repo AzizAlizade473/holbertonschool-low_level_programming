1e9e6bf8f73c65497888b5498982b3e1383ff212
AzizAlizade473 <elizaeeziz2006@gmail.com>
2026-01-08 14:14:58 +0000

    fdf

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to measure
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/* The loop iterates until it hits the null terminator */
+	}
+
+	return (i);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-#endif // MAIN_H_INCLUDED
-
+int _strlen(char *s);
+#endif /* MAIN_H */
