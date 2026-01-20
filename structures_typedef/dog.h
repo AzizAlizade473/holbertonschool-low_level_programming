3fb50e4163cbeb31019ee4137b100f1dd0ee407c
AzizAlizade473 <elizadeeziz2006@gmail.com>
2026-01-20 11:19:32 +0000

    struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,6 +15,13 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * typedef - another way of building struct
+ * @d: pointer to the struct dog to initialize
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 typedef struct
 {
 	char *name;
