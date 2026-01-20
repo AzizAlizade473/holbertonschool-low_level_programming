5531e3a72976aab9a669e4992d527a9e04078edb
AzizAlizade473 <elizadeeziz2006@gmail.com>
2026-01-20 21:12:12 +0000

    struct

---
+++ function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*func)(int, int);
+
+	/* 1. Check for correct number of arguments */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+
+	/* 2. Check if operator is valid */
+	if (func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	/* 3. Check for division/modulo by zero */
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
