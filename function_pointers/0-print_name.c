acd42df31849c67c4f06aea2fc114d3666963cde
AzizAlizade473 <elizadeeziz2006@gmail.com>
2026-01-20 18:27:43 +0000

    struct

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name of the person
+ * @f: pointer to the printing function
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+        f(name);
+}
